8244f8bc17822bfe36926800bf4780c93d158cd3
Travis Haby <travishaby@gmail.com>
2015-10-26 18:37:54 -0600

    implemented validation on user model for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
 
-  
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                = auth_info.uid
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "valid user" do
+    it "must have a name" do
+      user = User.new(name: "Travis")
+
+      expect(user).to be_valid
+    end
+  end
+
+
+  context "invalid user" do
+    it "must have a name" do
+      user = User.new
+
+      expect(user).to_not be_valid
+    end
+  end
 end
