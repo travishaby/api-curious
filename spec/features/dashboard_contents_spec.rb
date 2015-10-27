4085c87b331eaa33e318fa0dc4ec0850e7d708d5
Travis Haby <travishaby@gmail.com>
2015-10-26 22:28:24 -0600

    added additional feature test for politician columns

--- spec/features/dashboard_contents_spec.rb
+++ spec/features/dashboard_contents_spec.rb
@@ -7,13 +7,32 @@ RSpec.feature "DashboardContents", type: :feature do
     login_user
   end
 
-  scenario "dashboard has a navbar with appropriate links" do
+  scenario "navbar has appropriate links" do
     expect(page).to have_css("#navbar")
     within("#navbar") do
-      expect(page).to have_content("Logged in as travishaby")
+      expect(page).to have_content("Hello, travishaby")
       expect(page).to have_content("Logout")
       expect(page).to have_content("Policurious")
     end
+    click_on "Logout"
+    within("#navbar") do
+      expect(page).to_not have_content("Hello, travishaby")
+      expect(page).to_not have_content("Logout")
+      expect(page).to have_content("Login")
+    end
   end
-  
+
+  scenario "body has columns for national and local data" do
+    expect(page).to have_css("#state-politicians")
+    expect(page).to have_css("#national-politicians")
+    within("#state-politicians") do
+      expect(page).to have_content("Your State Representatives")
+      expect(page).to have_content("Your State Senators")
+    end
+    within("#national-politicians") do
+      expect(page).to have_content("Your National Representatives")
+      expect(page).to have_content("Your National Senators")
+    end
+  end
+
 end
