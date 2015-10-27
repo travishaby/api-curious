c0a915ae35d235dde35dee05099b6baa79485431
Travis Haby <travishaby@gmail.com>
2015-10-26 18:38:21 -0600

    oauth via twitter now working on local host, all tests passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
   def create
-    binding.pry
     if user = User.from_omniauth(request.env["omniauth.auth"])
       session[:user_id] = user.id
     end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+if Rails.env == "development"
+  YAML.load_file("#{::Rails.root}/config/application.yml")[::Rails.env].each {|k,v| ENV[k] = v }
+end
 # Initialize the Rails application.
 Rails.application.initialize!
