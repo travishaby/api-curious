04675290b12cb4747b6e54f39c3cb42e61af2837
Travis Haby <travishaby@gmail.com>
2015-10-29 10:12:12 -0600

    updated tests to reflect dashboard formatting changes

--- spec/features/dashboard_contents_spec.rb
+++ spec/features/dashboard_contents_spec.rb
@@ -26,11 +26,11 @@ RSpec.feature "DashboardContents", type: :feature do
     expect(page).to have_css("#state-legislators")
     expect(page).to have_css("#national-congresspeople")
     within("#state-legislators") do
-      expect(page).to have_content("State Representatives")
-      expect(page).to have_content("State Senators")
+      expect(page).to have_content("State Representative")
+      expect(page).to have_content("State Senator")
     end
     within("#national-congresspeople") do
-      expect(page).to have_content("National Representatives")
+      expect(page).to have_content("National Representative")
       expect(page).to have_content("National Senators")
     end
   end
