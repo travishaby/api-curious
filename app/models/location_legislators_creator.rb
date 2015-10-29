89531b027730669113a04d5e8ac7e7488d0973b9
Travis Haby <travishaby@gmail.com>
2015-10-28 20:42:58 -0600

    changed validation method for uniqueness of legislator to accomodate both API endpoint data sets for creating legislators

--- app/models/location_legislators_creator.rb
+++ app/models/location_legislators_creator.rb
@@ -6,17 +6,33 @@ class LocationLegislatorsCreator
     @service ||= SunlightService.new
   end
 
-  def create_state_legislators
+  def create_legislators
     service.state_legislators_by_location(current_user).each do |params|
       legislator = Legislator.find_or_create_by(full_name: params[:full_name],
                                     first_name: params[:first_name],
                                      last_name: params[:last_name],
-                                        leg_id: params[:first_name],
+                                  votesmart_id: params[:votesmart_id].to_s,
+                                        leg_id: params[:leg_id],
                            transparencydata_id: params[:transparencydata_id],
                                          level: params[:level],
                                        chamber: params[:chamber],
                                          party: params[:party],
                                          email: params[:email])
+
+      associate_user_with_legislator(legislator)
+    end
+
+    service.national_legislators_by_location(current_user).each do |params|
+
+      legislator = Legislator.find_or_create_by(first_name: params[:first_name],
+                                     last_name: params[:last_name],
+                                  votesmart_id: params[:votesmart_id].to_s,
+                                       fec_ids: params[:fec_ids],
+                                         level: "national",
+                                         phone: params[:phone],
+                                       chamber: params[:chamber],
+                                         party: params[:party],
+                                         email: params[:oc_email])
 
       associate_user_with_legislator(legislator)
     end
