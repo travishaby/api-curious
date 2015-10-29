81ef4291eb04acc3cfedea9a3ac7cd2327b26eb4
Travis Haby <travishaby@gmail.com>
2015-10-28 23:43:11 -0600

    legislators bills now are visible below each. feature tests verify functionality, all tests passing.

--- spec/features/dashboard_shows_bills_passeds_spec.rb
+++ spec/features/dashboard_shows_bills_passeds_spec.rb
@@ -1,30 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature "DashboardShowsBillsPassed", type: :feature, vcr: true do
-  xscenario "can see each politicians bills" do
+  scenario "can see each politicians bills" do
     stub_omniauth
     login_user_with_address
     visit root_path
-
-    within("#national-congresspeople") do
-      expect(page).to have_content("Cory")
-      within("#national-sen-bills") do
-        expect(page).to have_content("some bill")
-        expect(page).to_not have_content("some reps bill")
+    save_and_open_page
+    within("#state-legislators") do
+      expect(page).to have_content("Michael")
+      within("#johnston-bills") do
+        expect(page).to have_content("Police Data Collection And Community Policing")
       end
-      within("#national-rep-bills") do
-        expect(page).to have_content("some bill")
+      within("#mccann-bills") do
+        expect(page).to have_content("Education To Prevent Child Sexual Abuse & Assault")
       end
     end
 
-    within("#state-legislators") do
+    within("#national-congresspeople") do
       expect(page).to have_content("Cory")
-      within("#state-sen-bills") do
-        expect(page).to have_content("some bill")
-        expect(page).to_not have_content("some reps bill")
+      within("#michael-bennet-bills") do
+        expect(page).to have_content("Higher Education Innovation Act Congress")
+        expect(page).to_not have_content("Police Data Collection And Community Policing")
       end
-      within("#state-rep-bills") do
-        expect(page).to have_content("some bill")
+      within("#diana-degette-bills") do
+        expect(page).to have_content("Colorado Wilderness Act of 2015")
       end
     end
   end
